718473bd5239f057e1c5a15f7aa5c154dae8c566
Daniel Fernando Luna Sanchez <daniel.luna@paygo.cards>
2023-04-26 19:48:19 -0500

    Se realiza corrección del nombre de las apps en apps.py

--- prueba_cobrando_bpo/unidades_organizativas/apps.py
+++ prueba_cobrando_bpo/unidades_organizativas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UnidadesOrganizativasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'unidades_organizativas'
+    name = 'prueba_cobrando_bpo.unidades_organizativas'
