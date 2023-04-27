830283a9469670b6e2bb7a1d976d0fb04db02de0
Daniel Fernando Luna Sanchez <daniel.luna@paygo.cards>
2023-04-27 11:59:15 -0500

    Se elimina rutas de admin por defecto Django

--- prueba_cobrando_bpo/urls.py
+++ prueba_cobrando_bpo/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -32,7 +31,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('personas/', include('prueba_cobrando_bpo.personas.urls')),
     path('unidades-organizativas/', include('prueba_cobrando_bpo.unidades_organizativas.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
